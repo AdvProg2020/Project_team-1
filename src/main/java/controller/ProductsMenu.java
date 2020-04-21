c18f99c79517a970a06b65aef8da92e7bfe647c7
arian <arian.expert@gmail.com>
2020-04-22 00:10:07 +0430

    FilterCommandInProductMenu

--- src/main/java/controller/ProductsMenu.java
+++ src/main/java/controller/ProductsMenu.java
@@ -1,22 +1,49 @@
 package controller;
 
 import model.Commodity;
+import model.SuperMarket;
+import model.filter.Filter;
 
 import java.util.ArrayList;
 
 public class ProductsMenu implements CommandProcess {
 
+    private ArrayList<Filter> currentFilters = new ArrayList<Filter>();
+    private static ArrayList<Commodity> filteredCommodities = (ArrayList<Commodity>) SuperMarket.getAllCommodities().clone();
 
-    public void showAvailableFilters() {
+
+    public static void addFilteredCommodities(Commodity commodity) {
+        filteredCommodities.add(commodity);
     }
 
-    public void filter(String filter) {
+    public static void removeFromFilteredCommodities(Commodity commodity){
+        filteredCommodities.remove(commodity);
     }
 
-    public void printCommoditiesFeatures(ArrayList<Commodity> commodities) {
+    public void filter(Filter filter) {
+        currentFilters.add(filter);
+        updateFilteredCommodities();
     }
 
-    public void disableFilter() {
+    public void updateFilteredCommodities(){
+        for (Commodity commodity : SuperMarket.getAllCommodities()) {
+            if (canCommodityPassFilter(commodity))
+                filteredCommodities.add(commodity);
+        }
+    }
+
+    public boolean canCommodityPassFilter(Commodity commodity){
+        for (Filter filter : currentFilters) {
+            if (!filter.isCommodityMatches(commodity))
+                return false;
+        }
+        return true;
+    }
+
+
+    public void disableFilter(Filter filter) {
+            currentFilters.remove(filter);
+            updateFilteredCommodities();
     }
 
     public void sort(String sort) {
@@ -28,26 +55,12 @@ public class ProductsMenu implements CommandProcess {
     public void currentSort() {
     }
 
-    public void filterByName() {
-
-    }
-
-    public void filterByCategory() {
-
-    }
-
     private void sortByPrice() {
 
     }
 
     private void sortByScore() {
 
-    }
-
-    private void showProducts(){
-    }
-
-    private void showProduct(int productId){
     }
 
     @Override
--- src/main/java/model/SuperMarket.java
+++ src/main/java/model/SuperMarket.java
@@ -11,68 +11,75 @@ public class SuperMarket {
     private static ArrayList<Category> allCategory = new ArrayList<Category>();
     private static ArrayList<Commodity> allCommodities = new ArrayList<Commodity>();
     private static ArrayList<Score> allScores = new ArrayList<Score>();
-    private static ArrayList<Request> allRequests= new ArrayList<Request>();
+    private static ArrayList<Request> allRequests = new ArrayList<Request>();
     private static ArrayList<SimpleAccount> allAccounts = new ArrayList<SimpleAccount>();
 
-    public static ArrayList<SimpleAccount> getAllAccounts(){
-        return  allAccounts;
+    public static ArrayList<SimpleAccount> getAllAccounts() {
+        return allAccounts;
     }
-    public static ArrayList<TransactionLog> getAllLogs(){
+
+    public static ArrayList<TransactionLog> getAllLogs() {
         return allLogs;
     }
 
-    public static ArrayList<Category> getAllCategory(){
+    public static ArrayList<Category> getAllCategory() {
         return allCategory;
     }
 
-    public static ArrayList<Commodity> getAllCommodities(){
+    public static ArrayList<Commodity> getAllCommodities() {
         return allCommodities;
     }
 
 
-    public static ArrayList<Score> getAllScores(){
+    public static ArrayList<Score> getAllScores() {
         return allScores;
     }
 
-    public static ArrayList<Request> getAllRequests(){
+    public static ArrayList<Request> getAllRequests() {
         return allRequests;
     }
 
-    public static void addAccount(SimpleAccount account){
+    public static void addAccount(SimpleAccount account) {
         allAccounts.add(account);
     }
 
-    public static void addRequest(Request request){
+    public static void addRequest(Request request) {
         allRequests.add(request);
     }
 
-    public static void addLog(TransactionLog transactionLog){
+    public static void addLog(TransactionLog transactionLog) {
         allLogs.add(transactionLog);
     }
-    public static void addCategory(Category category){
+
+    public static void addCategory(Category category) {
         allCategory.add(category);
     }
-    public static void addCommodities(Commodity commodity){
+
+    public static void addCommodities(Commodity commodity) {
         allCommodities.add(commodity);
     }
-    public static void addScore(Score score){
+
+    public static void addScore(Score score) {
         allScores.add(score);
     }
-    public static Commodity getCommodityByName(String name){
+
+    public static Commodity getCommodityByName(String name) {
         for (Commodity commodity : allCommodities) {
             if (commodity.getName().equals(name))
                 return commodity;
         }
         return null;
     }
-    public static Commodity getCommodityById(int commodityId){
+
+    public static Commodity getCommodityById(int commodityId) {
         for (Commodity commodity : allCommodities) {
             if (commodity.getCommodityId() == commodityId)
                 return commodity;
         }
         return null;
     }
-    public static TransactionLog getLogByLogId(int logId){
+
+    public static TransactionLog getLogByLogId(int logId) {
         for (TransactionLog log : allLogs) {
             if (log.getLogId() == logId)
                 return log;
@@ -80,29 +87,29 @@ public class SuperMarket {
         return null;
     }
 
-    public static boolean isThereAnyAccountWithThisUserAndPassword(String user, String password){
+    public static boolean isThereAnyAccountWithThisUserAndPassword(String user, String password) {
         for (SimpleAccount account : allAccounts) {
-            if (checkPassword(account,user) && checkUser(account , user)){
+            if (checkPassword(account, user) && checkUser(account, user)) {
                 return true;
             }
         }
         return false;
     }
 
-    public static SimpleAccount getAccountByUserAndPassword(String user, String password){
+    public static SimpleAccount getAccountByUserAndPassword(String user, String password) {
         for (SimpleAccount account : allAccounts) {
-            if (checkPassword(account,user) && checkUser(account , user)){
+            if (checkPassword(account, user) && checkUser(account, user)) {
                 return account;
             }
         }
         return null;
     }
 
-    public static Boolean checkPassword(SimpleAccount account, String user){
+    public static Boolean checkPassword(SimpleAccount account, String user) {
         return account.getUsername().equals(user);
     }
 
-    public static Boolean checkUser(SimpleAccount account, String password){
+    public static Boolean checkUser(SimpleAccount account, String password) {
         return account.isPasswordCorrect(password);
     }
 
