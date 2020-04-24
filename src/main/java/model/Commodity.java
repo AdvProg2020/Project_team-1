0da1eed2f3ff17d73fd8e55c7c8ed32613351140
MohammadKashaniJabbari <h.kashani2001@gmail.com>
2020-04-24 21:52:18 +0430

    Fix Commodity

--- src/main/java/model/Commodity.java
+++ src/main/java/model/Commodity.java
@@ -1,6 +1,7 @@
 package model;
 
 import model.account.SimpleAccount;
+import model.field.Field;
 
 import java.util.ArrayList;
 
@@ -22,8 +23,8 @@ public class Commodity implements Requestable{
     private int numberOfScores;
 
     public Commodity(int commodityId,Status status, String brand, String name, int price,
-                     SimpleAccount seller, Boolean isCommodityAvailable, Category category, ArrayList<String> categorySpecifications,
-                     String description, int averageScore, int amount) {
+                     SimpleAccount seller, Boolean isCommodityAvailable, Category category,
+                     ArrayList<Field> categorySpecifications, String description, int averageScore, int amount) {
         this.commodityId = commodityId;
         this.status = status;
         this.brand = brand;
@@ -48,7 +49,7 @@ public class Commodity implements Requestable{
         isCommodityAvailable = commodityAvailable;
     }
 
-    public void setCategorySpecifications(ArrayList<String> categorySpecifications) {
+    public void setCategorySpecifications(ArrayList<Field> categorySpecifications) {
         this.categorySpecifications = categorySpecifications;
     }
 
@@ -119,7 +120,7 @@ public class Commodity implements Requestable{
         return category;
     }
 
-    public ArrayList<String> getCategorySpecifications() {
+    public ArrayList<Field> getCategorySpecifications() {
         return categorySpecifications;
     }
 
