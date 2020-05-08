bfbf9d635826bf4ba0e28e8d160374dce0e8abdb
MohammadKashaniJabbari <h.kashani2001@gmail.com>
2020-05-09 01:39:29 +0430

    modify viewInfo

--- src/main/java/commands/ViewPersonalInfoCommand.java
+++ src/main/java/commands/ViewPersonalInfoCommand.java
@@ -19,7 +19,10 @@ public class ViewPersonalInfoCommand extends Command {
     public String runCommand(String command) {
         this.menu = (AccountMenu) HandleMenu.getMenu();
         HandleMenu.setMenu(new ViewPersonalInfoMenu(SuperMarket.getOnlineAccount()));
-        return menu.getAccount().getInformation();
+        return menu.getAccount().getInformation() + "\nand enter the field you want to edit and after that the new" +
+                "value of that. For example: phone 09123456789\n" +
+                "only password is different and you have to edit it like this:\n" +
+                "password old_value new_value repeat_of_new_value";
     }
 
     public boolean checkCommand(String command) {
