783f5c3e863b3bb4b203b9942631fc6e5a44aebf
parsa <parsa2820@gmail.com>
2020-05-13 19:53:49 +0430

    Clean up shits 2

--- src/main/java/view/View.java
+++ src/main/java/view/View.java
@@ -10,7 +10,26 @@ public class View {
     public ArrayList<CommandProcess> allMenus = new ArrayList<CommandProcess>();
 
     public View() {
-        allMenus.add(new)
+        allMenus.add(new CreateDiscountCodeMenu());
+        allMenus.add(new CreateManagerMenu());
+        allMenus.add(new DigestMenu());
+        allMenus.add(new EditDiscountCodeMenu());
+        allMenus.add(new Filtering());
+        allMenus.add(new GetAccountInfoMenu());
+        allMenus.add(new GetCartMenu());
+        allMenus.add(new GetDiscountCodes());
+        allMenus.add(new LoginRegisterMenu());
+        allMenus.add(new ManageCategoryMenu());
+        allMenus.add(new ManageDiscountsMenu());
+        allMenus.add(new ManageProductsMenu());
+        allMenus.add(new ManageRequestMenu());
+        allMenus.add(new ManageRequestsMenu());
+        allMenus.add(new ManagerMenu());
+        allMenus.add(new ManageUsersMenu());
+        allMenus.add(new PersonalAccountMenu());
+        allMenus.add(new ProductMenu());
+        allMenus.add(new ProductsMenu());
+        allMenus.add(new ViewPersonalInfoMenu());
     }
 
     public void run(){
