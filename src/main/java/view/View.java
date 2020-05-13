b86b95061a5ce6c97f7ce9c722c698db91be1fba
arian <arian.expert@gmail.com>
2020-05-13 19:31:05 +0430

    Clean up shits

--- src/main/java/view/View.java
+++ src/main/java/view/View.java
@@ -1,4 +1,19 @@
 package view;
 
+import commands.business_panel.manage_off.AddOff;
+import controller.CommandProcess;
+
+import java.util.ArrayList;
+import controller.*;
+
 public class View {
+    public ArrayList<CommandProcess> allMenus = new ArrayList<CommandProcess>();
+
+    public View() {
+        allMenus.add(new)
+    }
+
+    public void run(){
+
+    }
 }
