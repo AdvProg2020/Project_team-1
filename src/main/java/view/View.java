8f79d047594587fd5b55ab8427cd3c60d570f169
MohammadKashaniJabbari <h.kashani2001@gmail.com>
2020-05-18 18:23:14 +0430

    update help commands

--- src/main/java/view/View.java
+++ src/main/java/view/View.java
@@ -12,7 +12,7 @@ import controller.reseller.ManageResellerOffsMenu;
 import controller.reseller.ManageResellerProductsMenu;
 import controller.reseller.ResellerMenu;
 import controller.share.*;
-import model.*;
+import model.Session;
 import model.account.BusinessAccount;
 import model.account.ManagerAccount;
 import model.account.PersonalAccount;
@@ -157,6 +157,10 @@ public class View {
                 comments();
             } else if (command.matches("sort comments by (?<field>\\w+)")) {
                 sortComments(command);
+            } else if (command.equals("back")) {
+                commodityMenu.goToPreviousMenu();
+            } else if (command.equals("help")) {
+                commodityMenuHelp();
             } else System.out.println("invalid command");
         };
     }
@@ -1943,49 +1947,82 @@ public class View {
     }
 
     private void customerMenuHelp() {
-        System.out.println("back\n" +
-                "help\n" +
-                "products\n" +
-                "purchase\n" +
-                "view balance\n" +
-                "view cart\n" +
-                "view discount codes\n" +
-                "view orders\n" +
-                "view personal info");
+        System.out.println("1 - back\n" +
+                "2 - help\n" +
+                "3 - login\n" +
+                "4 - logout\n" +
+                "5 - products\n" +
+                "6 - purchase\n" +
+                "7 - register\n" +
+                "8 - sort discounts by []\n" +
+                "9 - sort orders by [discount|payed]\n" +
+                "10 - view balance\n" +
+                "11 - view cart\n" +
+                "12 - view discount codes\n" +
+                "13 - view orders\n" +
+                "14 - view personal info");
     }
 
     private void cartMenuHelp() {
-        System.out.println("back\n" +
-                "decrease [productId]\n" +
-                "increase [productId]\n" +
-                "help\n" +
-                "products\n" +
-                "purchase\n" +
-                "show products\n" +
-                "show total price\n" +
-                "view [productId]");
+        System.out.println("1 - back\n" +
+                "2 - decrease [productId]\n" +
+                "3 - increase [productId]\n" +
+                "4 - help\n" +
+                "5 - login\n" +
+                "6 - logout\n" +
+                "7 - products\n" +
+                "8 - purchase\n" +
+                "9 - register\n" +
+                "10 - show products\n" +
+                "11 - show total price\n" +
+                "12 - sort products by [average score|brand|id|name|number of scores|price|visits]\n" +
+                "13 - view [productId]");
     }
 
     private void orderMenuHelp() {
-        System.out.println("back\n" +
-                "help\n" +
-                "products\n" +
-                "rate [productId] [1-5]\n" +
-                "show order [orderId]");
+        System.out.println("1 - back\n" +
+                "2- help\n" +
+                "3 - login\n" +
+                "4 - logout\n" +
+                "5 - products\n" +
+                "6 - rate [productId] [1-5]\n" +
+                "7 - register\n" +
+                "8 - show order [orderId]");
     }
 
     private void digestMenuHelp() {
-        System.out.println("add to cart\n" +
-                "back\n" +
-                "help\n" +
-                "products");
+        System.out.println("1 - add to cart\n" +
+                "2 - back\n" +
+                "3 - help\n" +
+                "4 - login\n" +
+                "5 - logout\n" +
+                "6 - products\n" +
+                "7 - register");
     }
 
     private void commentsMenuHelp() {
-        System.out.println("add comment\n" +
-                "back\n" +
-                "help\n" +
-                "products");
+        System.out.println("1 - add comment\n" +
+                "2 - back\n" +
+                "3 - help\n" +
+                "4 - login\n" +
+                "5 - logout\n" +
+                "6 - products\n" +
+                "7 - register");
+    }
+
+
+    private void commodityMenuHelp() {
+        System.out.println("1 - attributes\n" +
+                "2 - back\n" +
+                "3 - comments\n" +
+                "4 - compare [productId]\n" +
+                "5 - digest\n" +
+                "6 - help\n" +
+                "7 - login\n" +
+                "8 - logout\n" +
+                "9 - products\n" +
+                "10 - register\n" +
+                "11 - sort comments by [content|title]");
     }
 
     private void logout() {
