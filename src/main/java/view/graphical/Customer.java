49d99073217c1a395d24cccf1ade14fff8f4dcd3
MohammadKashaniJabbari <h.kashani2001@gmail.com>
2020-06-12 16:11:56 +0430

    add customer labels

--- src/main/java/view/graphical/Customer.java
+++ src/main/java/view/graphical/Customer.java
@@ -1,14 +1,37 @@
 package view.graphical;
 
 import controller.customer.CustomerMenu;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import model.Session;
+import model.account.PersonalAccount;
 import view.commandline.View;
 
-public class Customer {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Customer implements Initializable {
     private final CustomerMenu customerMenu = View.customerMenu;
+    public Label usernameLabel;
+    public Label firstNameLabel;
+    public Label lastNameLabel;
+    public Label emailLabel;
+    public Label phoneLabel;
+    public Label balanceLabel;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        PersonalAccount account = (PersonalAccount) Session.getOnlineAccount();
+        usernameLabel.setText(account.getUsername());
+        firstNameLabel.setText(account.getFirstName());
+        lastNameLabel.setText(account.getLastName());
+        emailLabel.setText(account.getEmail());
+        phoneLabel.setText(account.getPhoneNumber());
+        balanceLabel.setText(String.valueOf(customerMenu.getBalance()));
+    }
 
     public void onBackButtonClick(MouseEvent mouseEvent) {
         customerMenu.goToPreviousMenu();
