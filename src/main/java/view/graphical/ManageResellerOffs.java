33fe268b29c807321cafd417f9e91bb8530a2797
parsa <parsa2820@gmail.com>
2020-06-25 12:12:22 +0430

    Add manage reseller off gui

---
+++ src/main/java/view/graphical/ManageResellerOffs.java
@@ -0,0 +1,32 @@
+package view.graphical;
+
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ManageResellerOffs implements Initializable {
+
+
+    public AnchorPane manageOffsAnchorPane;
+    public ToggleButton manageOffsSortOrderToggleButton;
+    public ChoiceBox manageOffsSortField;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        
+    }
+
+    public void onBackClick(MouseEvent mouseEvent) {
+    }
+
+    public void onSortButtonClick(MouseEvent mouseEvent) {
+    }
+
+    public void onAddOffClick(MouseEvent mouseEvent) {
+    }
+}
