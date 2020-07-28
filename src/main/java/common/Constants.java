832aafb8ae8443a49609d3740872d236cbe656b4
Arian <arian.expert@gmail.com>
2020-07-28 18:09:25 +0430

    port

--- src/main/java/common/Constants.java
+++ src/main/java/common/Constants.java
@@ -2,13 +2,14 @@ package common;
 
 public class Constants {
     public static String SERVER_IP = "0.tcp.ngrok.io";
-    public static int SERVER_PORT = 14763;
+    public static int SERVER_PORT = 12655;
     public static int SERVER_PORT_LOCAL = 8888;
     public static String FILE_SERVER_IP = "2.tcp.ngrok.io";
-    public static int FILE_SERVER_PORT = 17590;
+    public static int FILE_SERVER_PORT = 16544;
     public static int FILE_SERVER_PORT_LOCAL = 1234;
-    public static String BANK_SERVER_IP = "2.tcp.ngrok.io";
-    public static int BANK_SERVER_PORT = 17991;
+    public static String BANK_SERVER_IP = "0.tcp.ngrok.io";
+    public static int BANK_SERVER_PORT = 16508;
+            ;
     public static int BANK_SERVER_PORT_LOCAL = 9999;
     public static int FILE_BUFFER_SIZE = 8000;
 }
